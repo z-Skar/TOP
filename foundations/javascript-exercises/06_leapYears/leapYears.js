def1ae7063f393bb57830652f35c9071af75abd2
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-08-17 20:29:15 +0100

    Complete leapYears exercise

--- foundations/javascript-exercises/06_leapYears/leapYears.js
+++ foundations/javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+    const isCentury = year % 100 === 0;
+    if ((isCentury && !isYearDivisibleByFourHundred) || !isYearDivisibleByFour) return false
+    return true;
 };
 
 // Do not edit below this line
