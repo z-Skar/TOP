d00d3e7a74c1225b9921b50ee4a2ac9cc9fea730
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-08-17 20:06:37 +0100

    Complete sumAll exercise

--- foundations/javascript-exercises/05_sumAll/sumAll.js
+++ foundations/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) return "ERROR";
 
+    let sum = 0;
+    for (let i = Math.min(start, end); i <= Math.max(start, end); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
