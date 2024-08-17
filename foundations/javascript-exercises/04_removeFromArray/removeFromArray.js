7c8d11170f104be4c4054d09fe789b3ce8f4b24b
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-08-17 19:17:47 +0100

    Complete removeFromArray exercise

--- foundations/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ foundations/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+
+    for (let i = 0; i < elements.length; i++) {
+        while(array.indexOf(elements[i]) !== -1) {
+            array.splice(array.indexOf(elements[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
