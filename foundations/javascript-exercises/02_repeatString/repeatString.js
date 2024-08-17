2fdbdbda8bfbe2b533a056c7eceaf0520922aba9
z-Skar <oscar.borfotina.dobrovan@gmail.com>
2024-08-17 11:36:11 +0100

    Complete repeatString exercise

--- foundations/javascript-exercises/02_repeatString/repeatString.js
+++ foundations/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    
+    let repeatString = "";
+    for (let i = 0; i < num; i++) {
+        repeatString += string;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
