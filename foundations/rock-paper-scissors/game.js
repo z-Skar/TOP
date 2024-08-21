a369a5435f2e3aff5f9e125b4e6a71a6d702e3e2
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-08-21 20:56:31 +0100

    Create the Script of the game
    Create the script of the game considering the UI and the responsiveness.

--- foundations/rock-paper-scissors/game.js
+++ foundations/rock-paper-scissors/game.js
@@ -1,94 +1,113 @@
+const TITLE = document.querySelector("#title");
+const PLAYER_SCORE = document.querySelector("#playerScore");
+const COMPUTER_SCORE = document.querySelector("#computerScore");
+const BUTTONS = document.querySelectorAll(".options button");
+const PARAGRAPH = document.querySelector("#playerPick");
+const RESULT = document.querySelector("#result");
+const COMPUTER_CHOICE = document.querySelector("#computerChoice");
+let computerScore = 0, playerScore = 0;
+
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
-    switch (computerChoice) {
-        case 1:
-            return "Rock";
-        case 2:
-            return "Paper";
-        case 3:
-            return "Scissors";
-    }
+    return new Promise((resolve) => {
+        let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+        if (computerChoice === 1) {
+            COMPUTER_CHOICE.src = './resources/rock.jpg';
+            resolve("Rock");
+        } else if (computerChoice === 2) {
+            COMPUTER_CHOICE.src = './resources/paper.png';
+            resolve("Paper");
+        } else {
+            COMPUTER_CHOICE.src = './resources/scissors.png';
+            resolve("Scissors");
+        };
+    });
 }
 
 function getPlayerChoice() {
-    let playerChoice = prompt("Choose one of the options: Rock, Paper, Scissors.");
-    if (playerChoice.localeCompare('Rock', undefined, { sensitivity: 'base' }) === 0) {
-        return "Rock";
-    } else if (playerChoice.localeCompare('Paper', undefined, { sensitivity: 'base' }) === 0) {
-        return "Paper";
-    } else if (playerChoice.localeCompare('Scissors', undefined, { sensitivity: 'base' }) === 0) {
-        return "Scissors";
-    } else return getPlayerChoice();
-}
-
-let computerScore = 0, playerScore = 0;
+    return new Promise((resolve) => {
+        for (let button of BUTTONS) {
+            button.addEventListener("click", () => {
+                PARAGRAPH.textContent = "You made a pick";
+                for (let btn of BUTTONS) {  // Iterate through BUTTONS again, if the button in the new iteration is different from the clicked button, then that button becomes hidden.
+                    if (btn !== button) {
+                        btn.setAttribute("style", "visibility: hidden");
+                    }
+                }
+                setTimeout(() => {
+                    PARAGRAPH.textContent = "Take your pick";
+                    for (let btn of BUTTONS) { // Iterate through the BUTTONS again, similar to the upper iteration, if the button is different from the clicked button, them that button becomes visible.
+                        if (btn !== button) {
+                            btn.setAttribute("style", "visibility: visible");
+                        }
+                    }
+                }, 2000);
+                resolve(button.id);
+            });
+        };
+    });
+};
 
 function playRound(computerChoice, playerChoice) {
-    const victory = `You won! ${playerChoice} beats ${computerChoice}.`
-    const defeat = `You lost! ${computerChoice} beats ${playerChoice}.`
-    const draw = `It's a draw!`
+    const victory = `You won! ${playerChoice} beats ${computerChoice}.`;
+    const defeat = `You lost! ${computerChoice} beats ${playerChoice}.`;
+    const draw = `It's a draw!`;
 
     switch (computerChoice) {
         case "Rock":
             if (playerChoice === "Paper") {
-                console.log(victory)
                 playerScore++;
+                return victory;
             } else if (playerChoice === "Scissors") {
-                console.log(defeat)
                 computerScore++;
+                return defeat;
             } else {
-                console.log(draw)
+                return draw;
             }
-            break;
         case "Paper":
             if (playerChoice === "Rock") { 
-                console.log(defeat)
                 computerScore++;
+                return defeat;
             } else if (playerChoice === "Scissors") { 
-                console.log(victory)
                 playerScore++;
+                return victory;
             } else { 
-                console.log(draw) 
+                return draw;
             }
-            break;
         case "Scissors":
             if (playerChoice === "Rock") {
-                console.log(victory)
                 playerScore++;
+                return victory;
             } else if (playerChoice === "Paper") {
-                console.log(defeat)
                 computerScore++;
+                return defeat;
             } else {
-                console.log(draw)
+                return draw;
             }
-            break;
-    }
-}
+    };
+};
 
-function playGame() {
-    for (let round = 1; round <= 5; round++) {
-        playRound(getComputerChoice(), getPlayerChoice());
-    }
-    console.log("Computer Score: " + computerScore)
-    console.log("Player Score: " + playerScore)
-    
-    if (computerScore > playerScore) {
-        console.log("You lost the game.")
-    }
-    else if (computerScore < playerScore) {
-        console.log("You won the game!")
-    }
-    else {
-        console.log("The game is finished, it's a draw!")
-    }
-}
+async function playGame() {
+   for (let round = 1; playerScore < 5 && computerScore < 5; round++) {
+        TITLE.textContent = `ROUND ${round}`;
+        PLAYER_SCORE.textContent = `Your Score: ${playerScore}`;
+        COMPUTER_SCORE.textContent = `Opponent Score: ${computerScore}`;
+        let playerChoice =  await getPlayerChoice().then((choice) => choice);
+        let computerChoice = await getComputerChoice().then((choice) => choice);
+        RESULT.textContent = playRound(computerChoice, playerChoice);
+        setTimeout(() => {
+            COMPUTER_CHOICE.src = "./resources/V-sign.png";
+            RESULT.textContent = "";
+        }, 2000);
+    };
 
-const TITLE = document.querySelector("#title");
-const COMPUTER_SCORE = document.querySelector("#computerScore");
-const PLAYER_SCORE = document.querySelector("#playerScore");
+    if (playerScore > computerScore) {
+        RESULT.textContent = "You won the game!";
+    } else if (computerScore > playerScore) {
+        RESULT.textContent = "You lost the game.";
+    } else {
+        RESULT.textContent = "The game is finished, it's a draw!";
+    }
+};
 
-
-for (let round = 1; playerScore < 4 && computerScore < 4; round++) {
-    playRound(getComputerChoice(), getPlayerChoice());
-    TITLE.textContent = `ROUND ${round}`;
-}
+playGame();
