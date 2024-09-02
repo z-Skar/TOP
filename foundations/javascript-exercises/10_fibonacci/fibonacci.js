849b2acd4e3783fff64a7210b4a106289b25be26
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-09-02 18:08:59 +0100

    Complete fibonacci exercise.

--- foundations/javascript-exercises/10_fibonacci/fibonacci.js
+++ foundations/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = Number(number)
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+    
+    currentNumber = 1;
+    previousNumber = 0;
+    total = 1;
 
+    for (let i = 1; i < number; i++) {
+        total = currentNumber + previousNumber;
+        previousNumber = currentNumber;
+        currentNumber = total;
+    };
+    return total;
 };
 
 // Do not edit below this line
