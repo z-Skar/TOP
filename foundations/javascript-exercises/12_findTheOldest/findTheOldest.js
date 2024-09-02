2fd55451ef3b4baa8fd634063c9ece0ebaff6ee6
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-09-02 20:07:37 +0100

    Complete findTheOldest exercise.

--- foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestPersonAge;
+        let actualPersonAge;
+        
+        if (!oldest.yearOfDeath) {
+            oldestPersonAge = (new Date().getFullYear()) - oldest.yearOfBirth;
+        } else {
+            oldestPersonAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        };
 
-};
+        if (!person.yearOfDeath) {
+            actualPersonAge = (new Date().getFullYear()) - person.yearOfBirth;
+        } else {
+            actualPersonAge = person.yearOfDeath - person.yearOfBirth;
+        };
+
+        if (actualPersonAge > oldestPersonAge) {
+            oldest = person;
+        };
+        return oldest;
+    }, people[0]);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
