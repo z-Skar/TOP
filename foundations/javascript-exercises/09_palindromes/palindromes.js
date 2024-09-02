6feccec7946f22c46106ea443a9392d01ac8f7c8
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-09-02 16:06:41 +0100

    Complete palindromes exercise.

--- foundations/javascript-exercises/09_palindromes/palindromes.js
+++ foundations/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const reversedWord = word.split("").reverse().join("").replace(/\W/g, '');
+    if (reversedWord.toUpperCase() === word.toUpperCase().replace(/\W/g, '')) return true;
+    return false;
 };
 
 // Do not edit below this line
