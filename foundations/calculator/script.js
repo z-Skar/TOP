8504959092f87e00845e93ba22ce5b0bec9ef622
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-09-16 21:03:44 +0100

    Restraining double dots
    Now, the user can`t type 2 dots in the expression. In adition, the default value will always be 0.

--- foundations/calculator/script.js
+++ foundations/calculator/script.js
@@ -5,8 +5,13 @@ const BODY = document.querySelector("body");
 
 
 // Functions
-const backspace = () => INPUT_BAR.textContent = INPUT_BAR.textContent.slice(0, -1);
-const clearInputBar = () => INPUT_BAR.textContent = '';
+const clearInputBar = () => INPUT_BAR.textContent = '0';
+const backspace = () => {
+    if (INPUT_BAR.textContent !== '0') {
+        INPUT_BAR.textContent = INPUT_BAR.textContent.slice(0, -1);
+        if (INPUT_BAR.textContent === '') INPUT_BAR.textContent = '0';
+    };
+};
 
 const porcentage = (number) => {
     if (number.toString().length <= 8) {
@@ -15,20 +20,52 @@ const porcentage = (number) => {
     return INPUT_BAR.textContent = parseFloat(number.toFixed(8));
 };
 
+const isValidKey = (key) => {
+    return (
+        !isNaN(key) ||
+        key === '.' ||
+        key === 'Backspace' ||
+        key === 'Escape' ||
+        key === '%'
+    );
+};
+
+const dotLimit = (value) => { // Function that verifies if the dot limit is breaked.
+    let dotCount = 0;
+    for (let i = 0; i < INPUT_BAR.textContent.length; i++) {
+        if (INPUT_BAR.textContent[i] === '.') {
+            dotCount++;
+        }
+    };
+
+    if (dotCount === 1 && value === '.') return true;
+    return false;
+};
 
 // Event Listeners
 ['keydown', 'click'].forEach(event => BODY.addEventListener(event, button => {
     const BUTTON_CLICKED = button.target.value;
     const BUTTON_PRESSED = button.key;
 
-    if (BUTTON_PRESSED === 'Escape' || BUTTON_CLICKED === 'AC') {clearInputBar();}
-    else if (BUTTON_PRESSED === 'Backspace') {backspace();}
-    else if (BUTTON_PRESSED === '%'|| BUTTON_CLICKED === '%') {porcentage(Number(INPUT_BAR.textContent / 100))}
-    else if (INPUT_BAR.textContent.length <= 8 && ((!isNaN(BUTTON_PRESSED) || BUTTON_PRESSED === '.') || (!isNaN(BUTTON_CLICKED) || BUTTON_CLICKED === '.'))) {
-        if (event === 'click') {
-            INPUT_BAR.textContent += BUTTON_CLICKED;
+    const INPUT = event === 'click' ? BUTTON_CLICKED : BUTTON_PRESSED
+    
+    if ((event === 'keydown' && !isValidKey(BUTTON_PRESSED)) || (BUTTON_PRESSED === ' ')) {
+        button.preventDefault();
+        return;
+    }
+
+    if (INPUT === 'Escape' || INPUT === 'AC') {
+        clearInputBar();
+    } else if (INPUT === 'Backspace') {
+        backspace();
+    } else if (INPUT === '%') {
+        porcentage(Number(INPUT_BAR.textContent / 100));
+    } else if (INPUT_BAR.textContent.length <= 8 && !dotLimit(INPUT) && (isValidKey(INPUT))) { 
+        // Limit of 8 numbers (including the dot) in the calculator.
+        if (INPUT_BAR.textContent === '0' && INPUT !== '.') {
+            INPUT_BAR.textContent = INPUT;
         } else {
-            INPUT_BAR.textContent += BUTTON_PRESSED;
-        };
+            INPUT_BAR.textContent += INPUT;
+        }
     };
 }));
