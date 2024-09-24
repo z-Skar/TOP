b5f47ed9e3c2e9829fb527af2a0d88194a8fd5c8
Óscar Dobrovan <oscar.borfotina.dobrovan@gmail.com>
2024-09-24 19:30:13 +0100

    Make the calculator be able to do math
    Now the calculator is fully devolped to do math through addition, subtraction, division and multiplication operations with the Keyboard or mouse.
    The code was a little refactor by the addition of some functions like the keyValidation to make the code in the event Listenner more digestable.

--- foundations/calculator/script.js
+++ foundations/calculator/script.js
@@ -2,10 +2,19 @@
 const INPUT_BAR = document.querySelector(".input-bar");
 const CALCULATOR = document.querySelector("body");
 
+// Variables
+let firstTerm = '', secondTerm = '';
+let operation = '', lastOperation = '';
 
 // Functions
-const clearInputBar = () => INPUT_BAR.textContent = '0';
 const invertSignal = () => INPUT_BAR.textContent = Number(INPUT_BAR.textContent) * (-1);
+const clearInputBar = () => {
+    INPUT_BAR.textContent = '0';
+    firstTerm = '';
+    secondTerm = '';
+    operation = '';
+    lastOperation = '';
+};
 
 const backspace = () => {
     if (INPUT_BAR.textContent !== '0') {
@@ -15,7 +24,7 @@ const backspace = () => {
 };
 
 const porcentage = (number) => {
-    number = Number(number / 100);
+    number = number / 100;
     if (number.toString().length < 9) {
         return INPUT_BAR.textContent = number;
     };
@@ -23,23 +32,53 @@ const porcentage = (number) => {
 };
 
 const isValidKey = (key) => {
-    return (
-        !isNaN(parseInt(key)) ||
-        key === 'Backspace' ||
-        key === 'Escape' ||
-        key === '+/-' ||
-        key === '%' ||
-        key === '.'
-    );
+    return (!isNaN(parseInt(key)) || ['Backspace', 'Escape', '+/-', '%', '.'].includes(key));
+};
+
+const isValidOperation = (key) => {
+    return (['+', '-', '*', '/', 'Enter'].includes(key));
+};
+
+const keyValidation = (key) => { // This function only returns true if the INPUT limit isn't broken nor the dot limit (1). The input must be a number.
+    if (INPUT_BAR.textContent.length < 10 && !dotLimit(key) && (!isNaN(parseInt(key)) || key === '.')) {
+        return true;
+    };
+    return false;
 };
 
 const dotLimit = (value) => { // Function that verifies if the dot limit was broken.
     for (let i = 0; i < INPUT_BAR.textContent.length; i++) {
-        if (INPUT_BAR.textContent[i] === '.' && value === '.') {
-            return true
+        if (INPUT_BAR.textContent[i] === '.' && value === '.') { // The input bar can only have 1 dot.
+            return true;
         };
     };
     return false;
+};
+
+const doMath = (firstTerm, operation, secondTerm) => {
+    let result;
+    switch(operation) {
+        case '+':
+            result = (firstTerm + secondTerm).toString();
+            break;
+        case '-':
+            result = (firstTerm - secondTerm).toString();
+            break;
+        case '*':
+            result = (firstTerm * secondTerm).toString();
+            break;
+        case '/':
+            result = (firstTerm / secondTerm).toString();
+            break;
+    };
+
+    if (result.includes('.'))
+    {
+        if (result.length > 9) {
+            return Number(result).toFixed(7);
+        };
+    };
+    return result;
 };
 
 
@@ -50,10 +89,30 @@ const dotLimit = (value) => { // Function that verifies if the dot limit was bro
 
     const INPUT = event === 'click' ? BUTTON_CLICKED : BUTTON_PRESSED;
     
-    if (event === 'keydown' && !isValidKey(BUTTON_PRESSED)) {
+    // If the key or operator isn't valid, then it won't be allowed to behave.
+    if (event === 'keydown' && !(isValidKey(BUTTON_PRESSED) || isValidOperation(BUTTON_PRESSED))) {
         button.preventDefault();
         return;
-    }
+    };
+
+    if (keyValidation(INPUT)) {
+        if ((INPUT_BAR.textContent === '0' && INPUT !== '.') || operation !== '') {
+            INPUT_BAR.textContent = INPUT;
+            lastOperation = operation;
+            operation = '';
+        } else {
+            INPUT_BAR.textContent += INPUT;
+        };
+    };
+
+    if (isValidOperation(INPUT)) {
+        if (INPUT !== 'Enter') operation = INPUT;
+        if (firstTerm === '') {
+            firstTerm = Number(INPUT_BAR.textContent);
+        } else {
+            secondTerm = Number(INPUT_BAR.textContent);
+        };
+    };
 
     switch (INPUT) {
         case 'Escape':
@@ -68,13 +127,9 @@ const dotLimit = (value) => { // Function that verifies if the dot limit was bro
         case '%':
             porcentage(Number(INPUT_BAR.textContent));
             return;
-    };
-
-    if (INPUT_BAR.textContent.length < 9 && !dotLimit(INPUT) && (isValidKey(INPUT))) {
-        if (INPUT_BAR.textContent === '0') {
-            INPUT_BAR.textContent = INPUT;
-        } else {
-            INPUT_BAR.textContent += INPUT;
-        }
+        case 'Enter':
+            INPUT_BAR.textContent = doMath(firstTerm, operation || lastOperation, secondTerm || Number(INPUT_BAR.textContent));
+            firstTerm = '';
+            return;
     };
 }));
